28254d6e1f99a551960ab7c5d3ee40a9cdfde01d
David England <david.england.de@googlemail.com>
2026-01-25 08:10:17 +0000

    Mount on the GPU

--- nn_attempt1.py
+++ nn_attempt1.py
@@ -6,10 +6,14 @@ from torch.utils.data import DataLoader, random_split
 
 
 def train(
-    net: nets.EuroNN, loss_fn: nn.Module, optimiser: optim.Optimizer, loader: DataLoader
+    net: nets.EuroNN,
+    loss_fn: nn.Module,
+    optimiser: optim.Optimizer,
+    loader: DataLoader,
+    dvc: torch.device,
 ):
     for i, batch in enumerate(loader):
-        x, t = batch
+        x, t = [g.to(dvc) for g in batch]
 
         optimiser.zero_grad()
 
@@ -23,13 +27,13 @@ def train(
             print(f"PROGRESS: {i+1}/{size_train}")
 
 
-def test(net: nets.EuroNN, loader: DataLoader):
+def test(net: nets.EuroNN, loader: DataLoader, dvc: torch.device):
     print("EVALUATING")
     correct, total = 0, 0
 
     with torch.no_grad():
         for batch in loader:
-            x, t = batch
+            x, t = [g.to(dvc) for g in batch]
 
             y = net(x)
             _, preds = torch.max(y, 1)
@@ -39,7 +43,16 @@ def test(net: nets.EuroNN, loader: DataLoader):
     print(f"ACCURACY: {correct}/{total}; {correct/total*100.:.2f}%")
 
 
+def get_device():
+    if torch.accelerator.is_available():
+        return torch.accelerator.current_accelerator()
+    else:
+        return torch.device("cpu")
+
+
 torch.manual_seed(4)
+
+dvc = get_device()
 
 dset = torchvision.datasets.EuroSAT(
     "/eurosatd", download=True, transform=torchvision.transforms.ToTensor()
@@ -52,8 +65,8 @@ loader_test = DataLoader(dset_test, batch_size=15, shuffle=True)
 size_train = len(loader_train)
 size_test = len(loader_test)
 
-net = nets.EuroNN()
+net = nets.EuroNN().to(dvc)
 print(net)
 
-train(net, nn.CrossEntropyLoss(), optim.SGD(net.parameters()), loader_train)
-test(net, loader_test)
+train(net, nn.CrossEntropyLoss(), optim.SGD(net.parameters()), loader_train, dvc)
+test(net, loader_test, dvc)
