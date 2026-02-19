07166256bbf326c1c5e260b2072b2035a714db32
David England <david.england.de@googlemail.com>
2026-02-19 08:50:43 +0000

    Add more experiments

--- experiments.py
+++ experiments.py
@@ -9,3 +9,27 @@ with open("default_settings.json", "r") as f:
         settings = defaults.copy()
         settings["batch_size"] = batch_size
         run(settings)
+
+    for epochs in [1, 2, 4, 5, 8, 16]:
+        print(f"\n>> EPOCHS {epochs}")
+        settings = defaults.copy()
+        settings["epochs"] = epochs
+        run(settings)
+
+    for kernel_size in [3, 4, 5, 8, 16]:
+        print(f"\n>> KERNEL SIZE {kernel_size}")
+        settings = defaults.copy()
+        settings["kernel_size"] = kernel_size
+        run(settings)
+
+    for num_conv_features in [1, 3, 5, 8, 16]:
+        print(f"\n>> NUM. CONV FEATURES {num_conv_features}")
+        settings = defaults.copy()
+        settings["num_conv_features"] = num_conv_features
+        run(settings)
+
+    for num_conv_layers in [1, 2, 3, 5]:
+        print(f"\n>> NUM. CONV LAYERS {num_conv_layers}")
+        settings = defaults.copy()
+        settings["num_conv_layers"] = num_conv_layers
+        run(settings)
